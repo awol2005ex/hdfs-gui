8fcf747ea9adad15dfee6632109cb370b22a8381
awol2005ex <awol2005ex@163.com>
2024-12-10 16:09:10 +0800

    增加路由

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+// src/router/index.ts
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Home from '../views/Home.vue';
+
+const routes: Array<RouteRecordRaw> = [
+{ path: '/', name: 'Home', component: Home },
+];
+
+const router = createRouter({
+history: createWebHistory(),
+routes,
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+
+<script setup lang="ts">
+import { reactive, Ref, ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+
+</script>
+
+<style scoped>
+</style>
