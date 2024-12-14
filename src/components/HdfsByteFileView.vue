ec00f5e66af1ef067e58424d09e27970e2646414
awol2005ex <awol2005ex@163.com>
2024-12-14 16:37:48 +0800

    Hdfs文件预览页面组件拆解

---
+++ src/components/HdfsByteFileView.vue
@@ -0,0 +1,102 @@
+<template>
+<Codemirror
+            v-model="codeValue"
+            :style="codeStyle"
+            :extensions="extensions"
+            v-bind="$attrs"
+            :disabled="true"
+            @ready="handleReady"
+            @change="onChange"
+            @focus="onFocus"
+            @blur="onBlur"
+          />
+</template>
+
+<script setup lang="ts">
+import { ref ,watch} from "vue";
+import type { CSSProperties } from "vue";
+import { Codemirror } from "vue-codemirror";
+import { vue } from "@codemirror/lang-vue";
+import { oneDark } from "@codemirror/theme-one-dark";
+import { get_file_preview_content } from "../api/hdfs_file.ts";
+interface Props {
+  codeStyle?: CSSProperties; // 代码样式
+  dark?: boolean; // 是否暗黑主题
+  code?: string; // 代码字符串
+  hdfsConfigId: number;
+  filePath: string;
+  // placeholder?: string // 占位文本
+  // autofocus?: boolean // 自动聚焦
+  // disabled?: boolean // 禁用输入行为和更改状态
+  // indentWithTab?: boolean // 启用tab按键
+  // tabSize?: number // tab按键缩进空格数
+}
+const props = withDefaults(defineProps<Props>(), {
+  // placeholder: 'Code goes here...',
+  codeStyle: () => {
+    return {};
+  },
+  dark: false,
+  code: "",
+  hdfsConfigId: 0,
+  filePath: "",
+  // autofocus: false,
+  // disabled: false,
+  // indentWithTab: true,
+  // tabSize: 2
+});
+const extensions = props.dark ? [vue(), oneDark] : [vue()];
+const codeValue = ref("");
+const emits = defineEmits(["update:code", "ready", "change", "focus", "blur"]);
+function handleReady(payload: any) {
+  // console.log('ready')
+  emits("ready", payload);
+}
+function onChange(value: string, viewUpdate: any) {
+  emits("change", value, viewUpdate);
+  emits("update:code", value);
+}
+function onFocus(viewUpdate: any) {
+  emits("focus", viewUpdate);
+}
+function onBlur(viewUpdate: any) {
+  emits("blur", viewUpdate);
+}
+
+const reloadFile= async () => {
+    try{ 
+         console.log(props)
+         if(props.hdfsConfigId >0 && props.filePath != ""){
+           const content = await get_file_preview_content( parseInt(props.hdfsConfigId as string) ,(props.filePath as string));
+   
+           codeValue.value = content;
+         }
+     }catch (error:any) {
+        ElMessage({
+          showClose: true,
+          message: error.toString(),
+          type: "error",
+        });
+
+    }
+}
+watch( ()=>props.id , (newId)=>{
+
+    reloadFile()
+  
+})
+reloadFile()
+</script>
+<style lang="less" scoped>
+:deep( .cm-editor) {
+  border-radius: 8px;
+  outline: none;
+  border: 1px solid transparent;
+  .cm-scroller {
+    border-radius: 8px;
+  }
+}
+:deep( .cm-focused ){
+  border: 1px solid fade(#000, 48%);
+}
+</style>
--- src/views/HdfsFileView.vue
+++ src/views/HdfsFileView.vue
@@ -49,19 +49,9 @@
         </el-breadcrumb>
       </el-header>
       <el-main>
-        <div>
+        <div v-if="mode == 'byte_preview'">
           <p>Preview File Bytes Content</p>
-          <Codemirror
-            v-model="codeValue"
-            :style="codeStyle"
-            :extensions="extensions"
-            v-bind="$attrs"
-            :disabled="true"
-            @ready="handleReady"
-            @change="onChange"
-            @focus="onFocus"
-            @blur="onBlur"
-          />
+          <HdfsByteFileView  :hdfsConfigId="route.params.id" :filePath="route.query.path" />
         </div>
       </el-main>
     </el-container>
@@ -76,57 +66,17 @@ import {
   HomeFilled,
 } from "@element-plus/icons-vue";
 
-import type { CSSProperties } from "vue";
-import { Codemirror } from "vue-codemirror";
-import { vue } from "@codemirror/lang-vue";
-import { oneDark } from "@codemirror/theme-one-dark";
 
 
-import { get_file_preview_content } from "../api/hdfs_file.ts";
+
 import {ElMessage  } from "element-plus";
+import HdfsByteFileView from "../components/HdfsByteFileView.vue";
 
-interface Props {
-  codeStyle?: CSSProperties; // 代码样式
-  dark?: boolean; // 是否暗黑主题
-  code?: string; // 代码字符串
-  // placeholder?: string // 占位文本
-  // autofocus?: boolean // 自动聚焦
-  // disabled?: boolean // 禁用输入行为和更改状态
-  // indentWithTab?: boolean // 启用tab按键
-  // tabSize?: number // tab按键缩进空格数
-}
-const props = withDefaults(defineProps<Props>(), {
-  // placeholder: 'Code goes here...',
-  codeStyle: () => {
-    return {};
-  },
-  dark: false,
-  // autofocus: false,
-  // disabled: false,
-  // indentWithTab: true,
-  // tabSize: 2
-});
-const extensions = props.dark ? [vue(), oneDark] : [vue()];
-const codeValue = ref("");
-const emits = defineEmits(["update:code", "ready", "change", "focus", "blur"]);
-function handleReady(payload: any) {
-  // console.log('ready')
-  emits("ready", payload);
-}
-function onChange(value: string, viewUpdate: any) {
-  emits("change", value, viewUpdate);
-  emits("update:code", value);
-}
-function onFocus(viewUpdate: any) {
-  emits("focus", viewUpdate);
-}
-function onBlur(viewUpdate: any) {
-  emits("blur", viewUpdate);
-}
 
 const router = useRouter();
 const route = useRoute();
 
+const mode=ref("byte_preview")
 //返回首页
 const backToHome = () => {
   router.push("/");
@@ -156,34 +106,8 @@ const current_parent_paths = ref(
   get_file_path_separator(route.query.path ? (route.query.path as string) : "/")
 );
 
-const reloadFile= async () => {
-    try{
-         const content = await get_file_preview_content( parseInt(route.params.id as string) ,(route.query.path as string));
-   
-         codeValue.value = content;
-     }catch (error:any) {
-        ElMessage({
-          showClose: true,
-          message: error.toString(),
-          type: "error",
-        });
-
-    }
-}
-
-reloadFile();
 </script>
 
-<style lang="less" scoped>
-:deep( .cm-editor) {
-  border-radius: 8px;
-  outline: none;
-  border: 1px solid transparent;
-  .cm-scroller {
-    border-radius: 8px;
-  }
-}
-:deep( .cm-focused ){
-  border: 1px solid fade(#000, 48%);
-}
+<style scoped>
+
 </style>
